196a2a938d099da918d998842665ca82a3f7b2f1
Aryan Goyal <aryang2000@gmail.com>
2021-06-09 12:04:24 -0400

    develop: added method signatures, added docstring to existing methods

--- rectangle.py
+++ rectangle.py
@@ -17,6 +17,27 @@ class Rectangle:
     def __str__(self):
         return "{},[[{},{}],[{},{}]]".format(self.name, str(self.top_left), str(self.top_right), str(self.bottom_right), str(self.bottom_left))
 
+    def width(self):
+        return self.top_right.x - self.top_left.x
+
+    def height(self):
+        return self.top_left.y - self.bottom_left.y
+        
     def intersects(self, other):
         # return not (self.top_right.x < other.bottom_left.x or self.bottom_left.x > other.top_right.x or self.top_right.y < other.bottom_left.y or self.bottom_left.y > other.top_right.y)
+        pass
+
+    def adjacentProper(self, other):
+        pass
+
+    def adjacentPartial(self, other):
+        pass
+
+    def adjacentSubline(self, other):
+        pass
+
+    def containment(self, other):
+        pass
+
+    def detectFeatures(self, other):
         pass
--- solution.py
+++ solution.py
@@ -4,12 +4,20 @@ from rectangle import Point
 from rectangle import Rectangle
 
 def read_json(file_name):
+    """
+    :type file_name: string
+    :rtype: dict
+    """
     f = open(file_name,)
     data = json.load(f)
     f.close()
     return data
 
 def generate_rectangle(points, name):
+    """
+    :type points: dict, name: string
+    :rtype: Rectangle
+    """
     top_left = Point(points[0][0], points[1][1])
     top_right = Point(points[1][0], points[1][1])
     bottom_right = Point(points[1][0], points[0][1])
@@ -17,12 +25,20 @@ def generate_rectangle(points, name):
     return Rectangle(top_left, top_right, bottom_right, bottom_left, name)
 
 def validate_rect(rect):
+    """
+    :type rect: dict
+    :rtype: None
+    """
     points = rect['points']
     err_msg = "Error: {} contains invalid points!".format(rect['name'])
     #assert that top right point is greater than bottom left point in x and y
     assert points[0][0] < points[1][0] and points[0][1] < points[1][1], err_msg
   
 def validate_json(rectangles):
+    """
+    :type rect: dict
+    :rtype: boolean
+    """
     valid = True
     for rect in rectangles:
         try:
@@ -32,15 +48,25 @@ def validate_json(rectangles):
             valid = False
     return valid
 
+def get_rectangles(data):
+    """
+    :type rect: dict
+    :rtype: list
+    """
+    rectangles = data['rectangles']
+    if validate_json(rectangles):
+        aggr = []
+        for rect in rectangles:
+            aggr.append(generate_rectangle(rect['points'], rect['name']))
+    
+        return aggr
+
+
 if __name__ == "__main__":
     data = read_json(sys.argv[1])
 
     if data:
-        rectangles = data['rectangles']
-        if validate_json(rectangles):
-            all_rectangles = []
-            for rect in rectangles:
-                all_rectangles.append(generate_rectangle(rect['points'], rect['name']))
-            
-            for x in all_rectangles:
-                print(x)   
+        rectangles = get_rectangles(data)
+
+        for x in rectangles:
+            print(x.width(), x.height())
